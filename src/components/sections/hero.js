1ee97c19cad0338a63ec900419c78bd3edd85e63
Adam Ananda Santoso <adamananda02@gmail.com>
2024-09-18 10:09:04 +0700

    refactor: udpate hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,22 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Adam Ananda Santoso.</h2>;
+  const three = <h3 className="big-heading">Powering Systems Behind the Scenes.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I'm a backend developer specializing in creating robust backend systems and databases, and
+        everything in between. Currently, working at{' '}
+        <a href="https://ypt.or.id/" target="_blank" rel="noreferrer">
+          Telkom Foundation
         </a>
         .
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
