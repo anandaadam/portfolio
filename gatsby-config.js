928c5a6bfaf841a43f08f4824204023fc8c3ade4
Adam Ananda Santoso <adamananda02@gmail.com>
2024-09-18 09:57:01 +0700

    refactor: update gatsby js config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Adam Ananda | Backend Developer',
+    description: 'Adam Ananda Santoso is a Backend Developer',
+    siteUrl: 'https://anandaadam.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
